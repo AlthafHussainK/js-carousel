9394a931ff47e6f168309f2f9a8d3f2a9c1e2160
AlthafHussainK <althafakm@gmail.com>
2021-03-04 08:36:17 +0530

    hideAllSlides function

--- js/index.js
+++ js/index.js
@@ -2,15 +2,32 @@
 
 const slides = document.getElementsByClassName('carousel-item')
 let slidePosition = 0
-
 const totalSlides = slides.length
 
 const nextButton = document.getElementById('carousel-button-next')
-nextButton.addEventListener('click', function moveToNextSlide(){
-  console.log('next')
-})
-
 const prevButton = document.getElementById('carousel-button-prev')
-prevButton.addEventListener('click', function moveToPrevSlide(){
-  console.log('prev')
-})  
+
+nextButton.addEventListener('click', moveToNextSlide)
+prevButton.addEventListener('click', moveToPrevSlide)  
+
+function hideAllSlides() {
+  for (const slide of slides){
+    slide.classList.remove('carousel-item-visible')
+    slide.classList.add('carousel-item-hidden')
+  }
+}
+
+function moveToNextSlide(){
+  hideAllSlides()
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0
+  } else {
+    slidePosition++
+  }
+  console.log(slidePosition)
+  slides[slidePosition].classList.add('carousel-item-visible')
+}
+
+function moveToPrevSlide(){
+
+}
